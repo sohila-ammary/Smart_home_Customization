64ff07a2b8507ca1a2626e4ca9a9d06e9aeb42d9
Sohailaa <108615899+sohila-ammary@users.noreply.github.com>
2026-05-24 20:13:07 +0200

    Create search_config.py

---
+++ src/search_config.py
@@ -0,0 +1,5 @@
+WINDOW_SIZE_GRID = ["3min", "5min", "10min"]
+MIN_LABEL_FRACTION_GRID = [0.5, 0.6, 0.7]
+MIN_CLASS_SAMPLES_GRID = [20, 30, 50]
+
+SEARCH_DATASETS = ["aruba", "cairo", "milan"]  # start with the weaker homes
